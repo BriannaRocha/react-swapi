d850485d70956e7b692852fa99a6493fb2f6c54b
BriannaRocha <rochabrianna17@gmail.com>
2024-02-05 23:51:01 -0500

    add starship details folder

---
+++ src/StarshipDetails/StarshipDetails.jsx
@@ -0,0 +1,14 @@
+// npm modules
+import { useEffect, useState } from "react"
+import { useParams } from "react-router-dom"
+
+// services
+import {  } from "../../services/api-calls"
+
+const StarshipDetails = () => {
+
+  return (
+    
+  )
+
+}
