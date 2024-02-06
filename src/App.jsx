bcd73e7a1586ff67610dab2ab26d18b71183fccc
BriannaRocha <rochabrianna17@gmail.com>
2024-02-06 00:32:51 -0500

    add api call to app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,28 @@
-import React, { Component } from 'react';
+// npm modules
+import { useState, useEffect } from "react"
+import React, { Component } from 'react'
 
+//css
 import './App.css'
 
+// components
+// import SearchForm from "../../components/SearchForm/SearchForm"
+// import SpellCard from "../../components/SpellCard/SpellCard"
+
+// services
+import { getAllStarships } from "../../services/sw-api"
+
 function App() {
+  const [starshipList, setStarshipList] = useState([])
+
+  useEffect(() => {
+    const fetchStarshipList = async () => {
+      const starshipData = await getAllStarships()
+      setStarshipList(starshipData)
+    }
+    fetchStarshipList()
+  }, [])
+
 
   return (
     <>
--- src/StarshipDetails/StarshipDetails.jsx
+++ src/StarshipDetails/StarshipDetails.jsx
@@ -3,12 +3,26 @@ import { useEffect, useState } from "react"
 import { useParams } from "react-router-dom"
 
 // services
-import {  } from "../../services/api-calls"
+import { getStarship } from "../../services/sw-api"
+
+
 
 const StarshipDetails = () => {
+  const [starshipDetails, setStarshipDetails] = useState({})
+  const { starshipId } = useParams()
+
+  useEffect(() => {
+    const fetchStarshipDetails = async () => {
+      const starshipData = await getStarshipDetails(starshipId)
+      setStarshipDetails(starshipData)
+    }
+    fetchStarshipDetails()
+  }, [starshipId])
 
   return (
-    
+
   )
 
 }
+
+export default StarshipDetails
